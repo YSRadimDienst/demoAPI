836320107044bb48abdfe415edef7b57c9c0ad7d
YSRadimDienst <yazan.sadieh@radimdienst.de>
2023-02-25 12:53:55 +0100

    init

---
+++ src/main/java/com/example/demo/controller.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import com.azure.core.annotation.Get;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/")
+    String get(){
+        return "Server is runnig";
+    }
+}
