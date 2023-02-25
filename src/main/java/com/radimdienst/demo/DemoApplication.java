767dbe19922bea24dec4ddfcf155d6d2aede2b86
YSRadimDienst <yazan.sadieh@radimdienst.de>
2023-02-25 22:53:26 +0100

    test

--- src/main/java/com/radimdienst/demo/DemoApplication.java
+++ src/main/java/com/radimdienst/demo/DemoApplication.java
@@ -2,10 +2,17 @@ package com.radimdienst.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController()
 public class DemoApplication {
 
+	@GetMapping()
+	String get()	{
+		return "test Erfolgreich!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
