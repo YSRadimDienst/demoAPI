ca254380b9b187873be196900bca6b45767f3cbc
YSRadimDienst <yazan.sadieh@radimdienst.de>
2023-02-27 09:20:56 +0100

    delete import

--- src/main/java/com/radimdienst/demo/DemoApplication.java
+++ src/main/java/com/radimdienst/demo/DemoApplication.java
@@ -1,18 +1,13 @@
 package com.radimdienst.demo;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Objects;
+
+
 
 @SpringBootApplication
 @RestController()
@@ -24,7 +19,7 @@ public class DemoApplication {
 	}
 
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 
 
 		SpringApplication.run(DemoApplication.class, args);
