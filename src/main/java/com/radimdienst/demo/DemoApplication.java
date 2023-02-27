8f1a8da5fcc2ee890dac62603d68f27a793fae15
YSRadimDienst <yazan.sadieh@radimdienst.de>
2023-02-27 09:12:55 +0100

    test ohne auto config

--- src/main/java/com/radimdienst/demo/DemoApplication.java
+++ src/main/java/com/radimdienst/demo/DemoApplication.java
@@ -1,9 +1,18 @@
 package com.radimdienst.demo;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController()
@@ -13,7 +22,11 @@ public class DemoApplication {
 	String get()	{
 		return "test Erfolgreich!!";
 	}
-	public static void main(String[] args) {
+
+
+	public static void main(String[] args) throws IOException {
+
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
